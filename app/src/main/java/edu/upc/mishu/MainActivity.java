9f57701ee07e824c46ef90a3a425fd83ee75921e
归零幻想 <yqmailsend@163.com>
2020-03-18 20:52:43 +0800

    初次打开，Liscense

--- app/src/main/java/edu/upc/mishu/MainActivity.java
+++ app/src/main/java/edu/upc/mishu/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.upc.mishu;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //第一次打开启动授权界面
+        SharedPreferences shared=getSharedPreferences("is", MODE_PRIVATE);
+        boolean isfer=shared.getBoolean("isfer", true);
+        SharedPreferences.Editor editor=shared.edit();
+        if(isfer){
+            //第一次进入跳转
+            Intent in=new Intent(MainActivity.this,LiscenceActivity.class);
+            startActivity(in);
+            finish();
+        }else{
+            //do nothing
+        }
     }
 }
