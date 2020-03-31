066070f035b7601f2cbb6bd8b19fdefa98328d5e
li <1214767274@qq.com>
2020-03-31 20:08:42 +0800

    Fragment
    Signed-off-by: li <1214767274@qq.com>

--- app/src/main/java/edu/upc/mishu/adapter/ViewPagerAdapter.java
+++ app/src/main/java/edu/upc/mishu/adapter/ViewPagerAdapter.java
@@ -1,40 +1,30 @@
 package edu.upc.mishu.adapter;
 
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.viewpager.widget.PagerAdapter;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 import java.util.List;
 
-public class ViewPagerAdapter extends PagerAdapter{
-    private List<View> viewlist;
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragmentList;
 
-    public ViewPagerAdapter (List<View> listview){
-        this.viewlist = listview;
-    }
-
-    @Override
-    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView(viewlist.get(position));//删除页卡
+    public  ViewPagerAdapter(List<Fragment> fragmentList, FragmentManager fragmentManager){
+        super(fragmentManager);
+        this.fragmentList = fragmentList;
     }
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        container.addView(viewlist.get(position),0);
-        return viewlist.get(position);
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return viewlist.size();
-    }
-
-    @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return view==object;
+        return fragmentList.size();
     }
 
 }
---
+++ app/src/main/java/edu/upc/mishu/fragment/SynchronousFragment.java
@@ -0,0 +1,31 @@
+package edu.upc.mishu.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import edu.upc.mishu.R;
+
+public class SynchronousFragment extends Fragment {
+    private static SynchronousFragment instance  = null;
+
+    public static SynchronousFragment newInstance(){
+        if(instance == null){
+            instance = new SynchronousFragment();
+        }
+        return instance;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_synchronous,container,false);
+        return view;
+
+    }
+}
